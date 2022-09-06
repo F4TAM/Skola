908776bbe24676a6fcd43c8a6c0431e2e1a09e78
F4TAM <91732905+F4TAM@users.noreply.github.com>
2022-09-06 12:33:31 +0200

    Add project files.

---
+++ Skola/src/SchoolSystem.cpp
@@ -0,0 +1,33 @@
+#include "SchoolSystem.h"
+
+void SchoolSystem::Run()
+{
+	int counter = 0;
+	while (true)
+	{
+		//Meny
+		//input
+		//Feedback
+		std::cout << "Loop: " << counter << "\n";
+		counter++;
+		std::cin.get(); 
+	}
+}
+
+void SchoolSystem::Meny()
+{
+	std::cout << "Welcome to schoolsystem " << "\n";
+}
+
+void SchoolSystem::AddStudent()
+{
+	Student student;
+	student.name = "blabal";
+	student.age = 123;
+	students.push_back(student);
+}
+
+void SchoolSystem::RemoveStudent()
+{
+
+}
---
+++ Skola/src/Student.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+
+
+
+struct Student
+{
+	std::string name = "";
+	int age = 0;
+	std::string Adress = ""; 
+	std::string number = "";
+};
